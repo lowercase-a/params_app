cbc1b82ae7229bf176709287c0c51fc6378cb2e4
Brian <riceb53@gmail.com>
2019-02-27 19:07:53 -0600

    change keys from strings to symbols in params

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,8 +1,8 @@
 class Api::PagesController < ApplicationController
   def query_action
     p "*" * 88
-    @name = params['name']
-    @the_job = params['job']
+    @name = params[:nayme]
+    @the_job = params[:job]
     p "*" * 88
     render 'query_view.json.jbuilder'
   end
